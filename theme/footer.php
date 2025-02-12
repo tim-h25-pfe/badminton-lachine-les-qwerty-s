11e4f33a3096ff188488d477c15fbd6f8209f03c
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-02-12 17:50:25 -0500

    dynamisation des menus dans le footer

--- theme/footer.php
+++ theme/footer.php
@@ -1,20 +1,61 @@
 <footer class="footer">
     <div class="wrapper">
-        <p>Copyright</p>
-        <nav>
-            <ul class="social">
-                <li><a href="#"><svg class="icon icon--md">
-                    <use xlink:href="#icon-facebook"></use>
-                </svg></a></li>
-                <li><a href="#"><svg class="icon icon--md">
-                    <use xlink:href="#icon-reddit"></use>
-                </svg></a></li>
-                <li><a href="#"><svg class="icon icon--md">
-                    <use xlink:href="#icon-youtube"></use>
-                </svg></a></li>
-            </ul>
-        </nav>
+
+        <?php wp_nav_menu(array(
+                'theme_location' => 'menu_header_vedette',
+                'container' => 'nav',
+                'container_class' => 'principal',
+            )); ?>
+
+
+        <?php wp_nav_menu(array(
+                'theme_location' => 'menu_header_sections',
+                'container' => 'nav',
+                'container_class' => 'secondary',
+            )); ?>
+
+        <div class="sociaux">
+            <div class="inscription">
+                <a class="btn_circled" href="#">
+                    inscrivez-vous
+                <svg class="icon">
+                    <use xlink:href="#icon-ovalDessin"></use>
+                </svg>
+                </a>
+                <a href="#" class="subscribe">Comment s'inscrire</a>
+            </div>
+            <div class="social">
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+            </div>
+        </div>
+
+        <div class="copyright">
+            <div class="contact">
+               <p>contact@badmintonlachine.com</p>
+                <p>2901, boulevard Saint-Joseph, Lachine H8S 4B7</p>
+            </div>
+            <div class="credits">
+                <a href="#">Politique de confidentialité</a>
+                <a href="#">Crédits</a>
+            </div>
+                <p>Tous droits réservés © 2025 Badminton Lachine</p>
+        </div>
+        
     </div>
+    <div class="passion">
+        <h1>Le badminton,</h1>
+        <h1>
+            notre
+            <span
+                >passion
+                <svg class="icon">
+                    <use xlink:href="#icon-ovalDessin"></use>
+                </svg>
+            </span>
+        </h1>
+    </div>
+   
 </footer>
 <?php wp_footer(); ?>
 </body>
--- theme/front-page.php
+++ theme/front-page.php
@@ -1,11 +1,120 @@
 <?php get_header(); ?>
 
     <section class="hero">
-    <img class="bgHero" src="<?php bloginfo('template_url') ?>/assets/images/hero.png" alt="" />
+    <div class="hero__media">
+        <img class="bgHero" src="<?php bloginfo('template_url') ?>/assets/images/heroAccueil.jpg" alt="" />
+        <div class="bkg_filter"></div>
+    </div>
     <div class="wrapper">
-            <h1>Ceci est la front-page, donc la page d'accueil</h1>
+        <div class="bigTitle">
+            <h1>Le badminton,</h1>
+            <h1>
+                notre
+                <span
+                    >passion
+                    <svg class="icon">
+                        <use xlink:href="#icon-doubleLigneDessin"></use>
+                    </svg>
+                </span>
+            </h1>
         </div>
-    </section>
+        <div class="hero_content">
+            <p>Fier d’être la plus grande institution de badminton sur l’île de Montréal depuis 2011!</p>
+            <a class="btn_circled" href="#"
+                >inscrivez-vous
+                <svg class="icon">
+                    <use xlink:href="#icon-ovalDessin"></use>
+                </svg>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="section partners">
+    <div class="wrapper">
+        <div class="swiper" data-autoplay data-loop data-freemode data-centered data-component="Carousel">
+            <div class="bkg_filter"></div>
+            <div class="swiper-wrapper">
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-conseilSportMontreal"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-doMore"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-badmintonQuebec"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-lachineMontreal"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-conseilSportMontreal"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-doMore"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-badmintonQuebec"></use>
+                    </svg>
+                </div>
+                <div class="swiper-slide icon--lg">
+                    <svg class="icon">
+                        <use xlink:href="#icon-lachineMontreal"></use>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section about">
+    <div class="wrapper">
+        <div class="bigCard">
+            <div class="about__content">
+                <h4>
+                    Nous sommes un organisme à but non lucratif qui a comme objectif d’être un acteur majeur du
+                    badminton à Montréal.
+                </h4>
+                <p>
+                    Que vous soyez un débutant ou un joueur expérimenté, nos programmes sont conçus pour vous aider à
+                    perfectionner votre technique et atteindre vos objectifs en badminton.
+                </p>
+                <a class="btn_full" href="#">qui sommes-nous ?</a>
+            </div>
+            <div class="about__stats">
+                <div class="stat">
+                    <h3>15 ans</h3>
+                    <p>D’expérience</p>
+                </div>
+                <div class="stat">
+                    <h3>10 000+</h3>
+                    <p>Joueurs accueilis</p>
+                </div>
+                <div class="stat">
+                    <h3>4</h3>
+                    <p>Professionnels</p>
+                </div>
+                <div class="stat">
+                    <h3>100%</h3>
+                    <p>Plaisir garanti</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
     <section>
         <div class="wrapper">
