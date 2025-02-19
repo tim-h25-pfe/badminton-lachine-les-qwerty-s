ef2eb6b03ab85f9bf5e2e515a89034d922aa2dff
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-02-19 09:43:32 -0500

    mise a jour de la page d'accueil statique dans le wordpress

--- theme/front-page.php
+++ theme/front-page.php
@@ -32,7 +32,14 @@
 
 <section class="section partners">
     <div class="wrapper">
-        <div class="swiper" data-autoplay data-loop data-freemode data-centered data-component="Carousel">
+        <div
+            class="swiper js-swiper-partenaire"
+            data-component="Carousel"
+            data-autoplay
+            data-loop
+            data-freemode
+            data-centered
+        >
             <div class="bkg_filter"></div>
             <div class="swiper-wrapper">
                 <div class="swiper-slide icon--lg">
@@ -84,6 +91,7 @@
     <div class="wrapper">
         <div class="bigCard">
             <div class="about__content">
+                <img class="traceVolant" src="<?php bloginfo('template_url') ?>/assets/images/traceVolant.png" alt="trace du volant" />
                 <h4>
                     Nous sommes un organisme à but non lucratif qui a comme objectif d’être un acteur majeur du
                     badminton à Montréal.
@@ -92,7 +100,7 @@
                     Que vous soyez un débutant ou un joueur expérimenté, nos programmes sont conçus pour vous aider à
                     perfectionner votre technique et atteindre vos objectifs en badminton.
                 </p>
-                <a class="btn_full" href="#">qui sommes-nous ?</a>
+                <a class="btn_outline" href="#">Qui sommes-nous ?</a>
             </div>
             <div class="about__stats">
                 <div class="stat">
@@ -116,31 +124,232 @@
     </div>
 </section>
 
-    <section>
-        <div class="wrapper">
-        <?php the_content(); ?>
-        <div class="cards">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <a href="<?php the_permalink(); ?>">
-                <div class="card">
-                    <div class="card__media">
-                    <?php the_post_thumbnail(); ?>
-                    </div>
-                    <div class="card__content">
-                    <!-- on peut get le php ici pour le titre de la page -->
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php the_excerpt(); ?></p>
-                        
+<section class="section services">
+    <div class="wrapper">
+        <div class="title">
+            <h2>
+                ser<span
+                    >vices
+                    <svg class="icon">
+                        <use xlink:href="#icon-tripleLigneDessin"></use>
+                    </svg>
+                </span>
+            </h2>
+            <a class="btn_full btn_top" href="#"
+                >Voir tout
+                <svg class="icon">
+                    <use xlink:href="#icon-fleche"></use>
+                </svg>
+            </a>
+        </div>
+        <div class="grid-services">
+            <div class="service">
+                <div class="service__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                </div>
+                <div class="service__content">
+                    <h5>Entraînement libre</h5>
+                    <div class="more__content">
+                        <a class="btn_full btn_white" href="#">En savoir plus </a>
                     </div>
                 </div>
-                </a>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_query(); ?>
-    </div>
+            </div>
+            <div class="service">
+                <div class="service__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                </div>
+                <div class="service__content">
+                    <h5>Entraînement libre</h5>
+                    <div class="more__content">
+                        <a class="btn_full btn_white" href="#">En savoir plus </a>
+                    </div>
+                </div>
+            </div>
+            <div class="service">
+                <div class="service__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                </div>
+                <div class="service__content">
+                    <h5>Entraînement libre</h5>
+                    <div class="more__content">
+                        <a class="btn_full btn_white" href="#">En savoir plus </a>
+                    </div>
+                </div>
+            </div>
+            <div class="service">
+                <div class="service__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                </div>
+                <div class="service__content">
+                    <h5>Entraînement libre</h5>
+                    <div class="more__content">
+                        <a class="btn_full btn_white" href="#">En savoir plus </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
+
+    <div
+        class="swiper carousel-services js-swiper-accueilServices"
+        data-component="Carousel"
+        data-loop
+        data-centered
+        data-coverflow
+    >
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="service">
+                    <div class="service__media">
+                        <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                    </div>
+                    <div class="service__content">
+                        <h5>Entraînement libres</h5>
+                        <div class="more__content">
+                            <a class="btn_full btn_white" href="#">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="service">
+                    <div class="service__media">
+                        <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                    </div>
+                    <div class="service__content">
+                        <h5>Entraînement libres</h5>
+                        <div class="more__content">
+                            <a class="btn_full btn_white" href="#">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="service">
+                    <div class="service__media">
+                        <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                    </div>
+                    <div class="service__content">
+                        <h5>Entraînement libres</h5>
+                        <div class="more__content">
+                            <a class="btn_full btn_white" href="#">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="service">
+                    <div class="service__media">
+                        <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+                    </div>
+                    <div class="service__content">
+                        <h5>Entraînement libres</h5>
+                        <div class="more__content">
+                            <a class="btn_full btn_white" href="#">En savoir plus </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <a class="btn_full btn_bottom" href="#"
+            >Voir tout
+            <svg class="icon">
+                <use xlink:href="#icon-fleche"></use>
+            </svg>
+        </a>
+    </div>
+</section>
+
+<section class="section nouvelles">
+    <div class="wrapper">
+        <div class="title">
+            <h2>
+                no<span
+                    >uvelles
+                    <svg class="icon">
+                        <use xlink:href="#icon-ligneDessin"></use>
+                    </svg>
+                </span>
+            </h2>
+            <a class="btn_full btn_white btn_top" href="#"
+                >Voir tout
+                <svg class="icon">
+                    <use xlink:href="#icon-fleche"></use>
+                </svg>
+            </a>
+        </div>
+        <div class="cards-nouvelles">
+            <div class="card">
+                <div class="card__content">
+                    <div class="text">
+                        <h5>Gala de la reconnaissance</h5>
+                        <p>Publié le 21 novembre 2024</p>
+                    </div>
+                    <a class="btn_full btn_round" href="#">
+                        <svg class="icon">
+                            <use xlink:href="#icon-fleche"></use>
+                        </svg>
+                    </a>
+                </div>
+                <div class="card__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/nouvelles.jpg" alt="image de la nouvelle" />
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__content">
+                    <div class="text">
+                        <h5>Gala de la reconnaissance</h5>
+                        <p>Publié le 21 novembre 2024</p>
+                    </div>
+                    <a class="btn_full btn_round" href="#">
+                        <svg class="icon">
+                            <use xlink:href="#icon-fleche"></use>
+                        </svg>
+                    </a>
+                </div>
+                <div class="card__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/nouvelles.jpg" alt="image de la nouvelle" />
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__content">
+                    <div class="text">
+                        <h5>Gala de la reconnaissance</h5>
+                        <p>Publié le 21 novembre 2024</p>
+                    </div>
+                    <a class="btn_full btn_round" href="#">
+                        <svg class="icon">
+                            <use xlink:href="#icon-fleche"></use>
+                        </svg>
+                    </a>
+                </div>
+                <div class="card__media">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/nouvelles.jpg" alt="image de la nouvelle" />
+                </div>
+            </div>
+        </div>
+        <a class="btn_full btn_white btn_bottom" href="#"
+            >Voir tout
+            <svg class="icon">
+                <use xlink:href="#icon-fleche"></use>
+            </svg>
+        </a>
+    </div>
+</section>
+
+<section class="section">
+    <div class="wrapper">
+        <a class="lien-contact" href="<?php bloginfo('url') ?>/contact">
+            <div class="lien-contact__content">
+                <h4>Besoin d'infos ?</h4>
+                <h2>parlons-en !</h2>
+            </div>
+            <div class="lien-contact__media"></div>
+        </a>
+    </div>
+</section>
     
 
 <?php get_footer(); ?>
