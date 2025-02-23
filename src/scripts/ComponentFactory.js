c9aef38eda67001cfbde340978bb23f58dee2905
Justin Valois <justin.x.valois@gmail.com>
2025-02-22 21:46:03 -0500

    Page a propos 100% intégrée
    La page à propos en version desktop, tablette et mobile est 100% intégrée. Le bloc accordéon est aussi 100% responsive

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -1,7 +1,7 @@
 import Carousel from './components/Carousel';
 import Header from './components/Header';
 import Tabs from './components/Tabs';
-
+import Accordeon from './components/Accordeon';
 export default class ComponentFactory {
   constructor() {
     this.componentInstances = [];
@@ -9,6 +9,7 @@ export default class ComponentFactory {
       Carousel,
       Header,
       Tabs,
+      Accordeon,
     };
     this.init();
   }
