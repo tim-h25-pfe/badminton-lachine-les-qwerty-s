6b1025e5a8d66e71916862b4d2ecaa4e40fe2bdd
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-02-24 09:00:50 -0500

    creation gabarits des blocs et debut cheat sheet

---
+++ theme/----cheat-sheet.php
@@ -0,0 +1,14 @@
+<?php the_title(); ?>
+<?php the_content(); ?>
+<?php the_excerpt(); ?>
+<?php the_post_thumbnail(); ?>
+<?php the_post_thumbnail_url(); ?>
+
+<?php bloginfo('url'); ?>
+<?php bloginfo('template_url') ?>
+<?php bloginfo('name') ?>
+<?php bloginfo('chartset') ?>
+
+<?php the_field('field_name'); ?>
+<?php the_sub_field('field_name'); ?>
+
