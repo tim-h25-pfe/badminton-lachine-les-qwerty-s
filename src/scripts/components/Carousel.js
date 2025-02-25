95e340853e4fb4a18428656c84c7c216b7d6707c
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-02-25 15:26:20 -0500

    fin correction blocs et structure html

--- src/scripts/components/Carousel.js
+++ src/scripts/components/Carousel.js
@@ -6,10 +6,19 @@ export default class Carousel {
     this.options = {};
     if (this.element.classList.contains('js-swiper-partenaire')) {
       this.options = {
-        slidesPerView: 3,
-        speed: 4000,
-        delay: 1,
-        spaceBetween: 0,
+        slidesPerView: 3, // Nombre de slides visibles (ajuste selon ton besoin)
+        spaceBetween: 300, // Espacement entre les slides
+        loop: true, // Active le mode boucle
+        speed: 5000, // Définit la vitesse du scroll (ajuste selon ton besoin)
+        autoplay: {
+          delay: 0, // Pas de pause entre les slides
+          disableOnInteraction: false, // Ne s'arrête pas au survol ou clic
+        },
+        centeredSlides: true,
+        freeMode: true, // Permet un glissement fluide et continu
+        momentum: true,
+        momentumBounce: false,
+        allowTouchMove: false, // Empêche le swipe manuel pour éviter des arrêts
       };
     } else if (this.element.classList.contains('js-swiper-accueilServices')) {
       this.options = {
@@ -76,6 +85,8 @@ export default class Carousel {
           },
         },
       };
+    } else {
+      console.log("pas d'options pour ce swiper");
     }
     this.init();
   }
