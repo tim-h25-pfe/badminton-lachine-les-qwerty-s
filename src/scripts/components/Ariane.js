b5d6a11e3e33f9006ff3aac2948936b990c50bec
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-03-03 10:59:42 -0500

    debut modifications ariane

--- src/scripts/components/Ariane.js
+++ src/scripts/components/Ariane.js
@@ -5,6 +5,7 @@ export default class Ariane {
       // rootMargin: '0px 0px -95% 0px',
       repeat: true,
     };
+    this.lastScrollY = window.scrollY; // Stocke la position initiale du scroll
     this.nav = this.element.querySelector('.ariane-nav');
     this.links = [];
     this.headings = this.element.querySelectorAll('[data-ariane-section]');
@@ -26,6 +27,9 @@ export default class Ariane {
       const heading = this.headings[i];
       observer.observe(heading);
     }
+    if (this.headings.length == 0) {
+      console.log('pas de section enregistrée');
+    }
   }
 
   initNav() {
@@ -42,7 +46,10 @@ export default class Ariane {
       link.href = `#${id}`;
       link.innerText = text;
 
-      this.nav.appendChild(link);
+      if (this.nav) {
+        this.nav.appendChild(link);
+      }
+
       this.links.push(link);
 
       this.createLink(link, heading, id);
@@ -55,32 +62,35 @@ export default class Ariane {
   }
 
   watch(entries) {
+    let scrollDown = window.scrollY > this.lastScrollY; // Détecte la direction
+    this.lastScrollY = window.scrollY; // Met à jour la position du scroll
+
     for (let i = 0; i < entries.length; i++) {
       const entry = entries[i];
       const target = entry.target;
       const link = target.getAttribute('data-link');
       const bond = this.element.querySelector(`[data-linked="${link}"]`);
-      if (entry.isIntersecting) {
+      if (entry.isIntersecting && scrollDown) {
         // -------- pour voir le seul actif ---------
 
-        // for (let i = 0; i < this.links.length; i++) {
-        //   const linke = this.links[i];
-        //   linke.classList.remove('active');
-        // }
-        // bond.classList.add('active');
-
-        //  ------- pour voir une progression -------
         for (let i = 0; i < this.links.length; i++) {
           const linke = this.links[i];
           linke.classList.remove('active');
         }
-        for (let i = 0; i < this.links.length; i++) {
-          const linke = this.links[i];
-          linke.classList.add('active');
-          if (linke == bond) {
-            return;
-          }
-        }
+        bond.classList.add('active');
+
+        //  ------- pour voir une progression -------
+        // for (let i = 0; i < this.links.length; i++) {
+        //   const linke = this.links[i];
+        //   linke.classList.remove('active');
+        // }
+        // for (let i = 0; i < this.links.length; i++) {
+        //   const linke = this.links[i];
+        //   linke.classList.add('active');
+        //   if (linke == bond) {
+        //     return;
+        //   }
+        // }
       }
     }
   }
--- theme/wp-config.php
+++ theme/wp-config.php
@@ -160,9 +160,7 @@ $table_prefix = 'wp_';
 
  */
 
-define( 'WP_DEBUG', true );
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
+define( 'WP_DEBUG', false );
 
 
 
