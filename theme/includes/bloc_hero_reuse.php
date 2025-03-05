024db1417f0aed4278ccd24c42a2da5c8ad815a6
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-03-05 10:23:36 -0500

    corrections bug qui empechait de cliquer sur les liens dans le footer

--- theme/includes/bloc_hero_reuse.php
+++ theme/includes/bloc_hero_reuse.php
@@ -1,20 +1,19 @@
+<div class="header-back"></div>
 <section class="hero block <?php the_field('hero_align'); ?>">
-    
-        <div class="hero__media">
+    <div class="hero_content">
+        <div class="texte">
+            <h1><?php the_title(); ?></h1>
+            <p>
+            <?php the_content(); ?>
+            </p>
+        </div>
+        <div class="hero-img">
         <?php 
         if (has_post_thumbnail()) { ?>
-            <img class="bgHero" src="<?php the_post_thumbnail_url(); ?>" alt="hero" />
+            <?php the_post_thumbnail(); ?>
             <?php  } else { ?>
-            <img class="bgHero" src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
+            <img src="<?php bloginfo('template_url') ?>/assets/images/cordageAccueilServices.jpg" alt="image de raquettes" />
         <?php } ?>
         </div>
-        <div class="hero__content">
-            <h1><?php the_title(); ?></h1>
-            <div class="underline">
-                <svg class="icon icon--lg">
-                    <use xlink:href="#icon-doubleLigneDessin"></use>
-                </svg>
-            </div>
-        </div>
-    
+    </div>
 </section>
