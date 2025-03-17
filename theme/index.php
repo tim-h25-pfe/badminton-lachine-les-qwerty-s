7b867b41c3f91d8d9da9d19d09f8f9ec77a7290c
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-03-17 11:49:01 -0400

    fait l'index.php

--- theme/index.php
+++ theme/index.php
@@ -1,36 +1,18 @@
 <?php get_header(); ?>
 
-    <section class="hero">
-        <img class="bgHero" src="<?php bloginfo('template_url') ?>/assets/images/hero.png" alt="" />
-        <div class="wrapper">
+<section class="hero block zero">
+    <div class="hero_content">
+        <div class="texte">
             <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
         </div>
-    </section>
-
-    <section>
-        <div class="wrapper">
-        <?php the_content(); ?>
-        <div class="cards">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <a href="<?php the_permalink(); ?>">
-                <div class="card">
-                    <div class="card__media">
-                    <?php the_post_thumbnail(); ?>
-                    </div>
-                    <div class="card__content">
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php the_excerpt(); ?></p>
-                        
-                    </div>
-                </div>
-                </a>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_query(); ?>
     </div>
-        </div>
-    </section>
-    
+</section>
+
+<section class="index">
+    <div class="wrapper">
+        <h3 style="text-align: center"><?php the_field('index_message', 'options'); ?></h3>
+    </div>
+</section>
 
 <?php get_footer(); ?>
