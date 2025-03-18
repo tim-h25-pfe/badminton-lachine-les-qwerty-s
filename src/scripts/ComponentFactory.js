bdc79d89dafd23dbbd5f9e02014831835938283e
EmileTardif31 <2241485@cegepmontpetit.ca>
2025-03-18 08:54:51 -0400

    modif de l'usine a composante pour recevoir un tableau et non un string

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -23,18 +23,25 @@ export default class ComponentFactory {
     };
     this.init();
   }
+
   init() {
     const components = document.querySelectorAll('[data-component]');
 
     for (let i = 0; i < components.length; i++) {
       const element = components[i];
-      const componentName = element.dataset.component;
 
-      if (this.componentList[componentName]) {
-        const instance = new this.componentList[componentName](element);
-        this.componentInstances.push(instance);
-      } else {
-        console.log(`La composante ${componentName} n'existe pas`);
+      // va chercher le data-component et peut get plusieurs composantes, et le transforme en tableau
+      const componentNames = element.dataset.component.split(/[\s,]+/);
+
+      for (let i = 0; i < componentNames.length; i++) {
+        const componentName = componentNames[i];
+
+        if (this.componentList[componentName]) {
+          const instance = new this.componentList[componentName](element);
+          this.componentInstances.push(instance);
+        } else {
+          console.log(`La composante ${componentName} n'existe pas`);
+        }
       }
     }
   }
